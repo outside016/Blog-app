c6097bb93c3e50fce238f137980e95728daed23e
outside016 <outside016@icloud.com>
2023-12-05 22:45:26 +0300

    verstka

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-post-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './post-page.component.html',
   styleUrl: './post-page.component.scss'
 })
